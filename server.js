c9b2939765036d184d0b1e23d9fd3578bf945c46
Your Name <you@example.com>
2021-03-31 23:49:25 +0300

    add

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, () => {
